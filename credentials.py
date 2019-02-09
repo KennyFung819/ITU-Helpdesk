385f82cb89edf9723038aa1b52b6d9c1c7f09cf9
KennyTsang <chufung97819@gmail.com>
2019-02-09 17:52:14 +0800

    Put Credential into .env files
    Remove credentials.py

--- credentials.py
+++
@@ -1,4 +0,0 @@
-ASSISTANT_APIKEY='K_1o3ADHk7x93vjYdrr2mobgfu7s-66eYceW-RPDfuFk'
-ASSISTANT_URL='https://gateway-tok.watsonplatform.net/assistant/api'
-ASSISTANT_VERSION = '2018-11-08'
-ASSISTANT_ID = '2ada3e75-f75c-4c72-97a3-cabe8f064d66'
