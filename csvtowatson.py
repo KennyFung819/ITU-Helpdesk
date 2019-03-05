01a7dee4e060521899abb0ccdaa89a3cff9a0037
Kenny Tsang <chufung97819@gmail.com>
2019-03-05 22:01:35 +0800

    Intent related function completed

--- csvtowatson.py
+++ csvtowatson.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import os
 
 global assistantV1
+
+
 try:
     load_dotenv('test.env')
     ASSISTANT_APIKEY = os.getenv("ASSISTANT_APIKEY")
@@ -55,20 +57,27 @@ def create_connection():
     print("Connected")
 
 #list all the existing intent
-def list_intent():
+def list_intent(intent_index):
     response=assistantV1.list_intents(
         workspace_id=WORKSPACE_ID
     ).get_result()
-    print(json.dumps(response, indent=2))
+    for item in response['intents']:
+        #put intent name into dict
+        intent_index.update({item['intent']:{}})
+    #return the dict   
+    print('Initalize the intent_list') 
+    return intent_index
 
 #get the selected intent
-def get_intent(intent):
+def get_intent(intent,intent_dict):
     response=assistantV1.get_intent(
         workspace_id=WORKSPACE_ID,
         intent=intent,
         export=True
     ).get_result()
-    print(json.dumps(response, indent=2))
+    intent_dict.update({intent:response['examples']})
+    print('Updated dict with ',intent)
+    return intent_dict
 
 #create a new intent
 def create_intent(intent,new_examples):
@@ -80,12 +89,12 @@ def create_intent(intent,new_examples):
     print(json.dumps(response, indent=2))
 
 #update the whole intent
-def update_intent(intent,new_examples):
+def update_intent(intent,updated_dict):
+    new_examples = updated_dict[intent]
     response=assistantV1.update_intent(
         workspace_id=WORKSPACE_ID,
         intent=intent,
-        new_examples=new_examples,
-        new_description='Updated intent'
+        new_examples=new_examples
     ).get_result()
     print(json.dumps(response, indent=2))
 
@@ -157,7 +166,7 @@ def create_node():
 
 #update the selected dialog node
 def update_node():
-    response=assistant.update_dialog_node(
+    response=assistantV1.update_dialog_node(
         workspace_id='{workspace_id}',
         dialog_node='greeting',
         new_output={
@@ -166,15 +175,34 @@ def update_node():
     ).get_result()
     print(json.dumps(response, indent=2))
 
+#controller example for intent
+def update_intent_sample():
+    #variable
+    intent_name = 'General_About_You'
+    exist_intent_dict = {}
+    # initalize the list
+    exist_intent_dict = list_intent(exist_intent_dict)
+    # get the select intent from Watson
+    exist_intent_dict = get_intent(intent_name,exist_intent_dict)
+    # update the list of that intent
+    update_list = exist_intent_dict[intent_name]
+    update_list.append({'text':'Test About You!!!'})
+    exist_intent_dict.update({intent_name:update_list})
+    print('Updating the list')
+    #Update on Watson
+    update_intent(intent_name,exist_intent_dict)
+    exist_intent_dict.clear()
+    
 if __name__ == "__main__":
     try:
-        #create_connection()
+        create_connection()
+        update_intent_sample()
         #create_entity()
         #update_intent('hello',[{'text':'good evening'}] )
         #update_entity()
         #create_node()
         #get_synonym()
         #create_synonym()
-        csv_controller()
+        #csv_controller()
     except Exception as e:
         print(e)
